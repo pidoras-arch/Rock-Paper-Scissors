5379da9a7d538de326b0b4c95c143eb33e70a0a2
Murtaza <murtaza.mod@gmail.com>
2025-07-07 15:27:02 +0300

    Create a function for the player choice
    through showing a prompt asking the user to write their choice of
    paper, rock or scissors

---
+++ script.js
@@ -0,0 +1,14 @@
+/* 
+pseudocode:
+- Create a prompt to ask the user for their choice (rock, paper, or scissors)
+- Generate a random choice for the computer (rock, paper, or scissors)
+- Compare the user's choice with the computer's choice
+- Determine the winner based on the rules of rock-paper-scissors
+- Display a message telling the user wether he won, lost or tied
+- Ask the user if he wants to play again
+*/
+const choices = ["rock", "paper", "scissors"]
+
+function askForChoice(){
+    return prompt("Enter your choice 'paper, rock, or scissors'").toLowerCase;
+}
